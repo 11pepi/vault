f1145deb0f26e26857bb67b7246ca0932aec891f
Colton Wollinger <cwoll1home@gmail.com>
2025-07-12 18:29:50 -0400

    Changed setup to reflect more recent vault design changes.

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
-#! /bin/python
+'''
+handles setting up and removing vault
+'''
+#!/bin/python
 import os
-import sys
+
 def set_up_vault(reset_i_think):
     '''
     set up vault for the first time
     '''
-    os.system("mkdir /vault &> /dev/null")
     try:
-        with open("/vault/pkglist","x") as f:
-            f.write("vault")
+        # safer than system()
+        os.makedirs("/globals/vault/db")
     except Exception:
         if not reset_i_think:
             print("Please don't run setup multiple times!")
         else:
-            print("Remove... /vault")
-            os.system("sudo rm -rf /vault")
+            print("Remove... /globals/vault/db")
+            os.system("sudo rm -rf /globals/vault/db")
             print("Done!")
             set_up_vault(False)
-
-if __name__ == "__main__":  # prevent running this code on module import
-    try:
-        set_up_vault(sys.argv[1] == "reset")
-    except IndexError:
-        set_up_vault(False)
